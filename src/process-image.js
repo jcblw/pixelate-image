c90373e1184b278f985510d75883e5ecb1dfd04b
jcblw <578259+jcblw@users.noreply.github.com>
2020-06-02 09:10:34 -0700

    add in stuff

--- src/process-image.js
+++ src/process-image.js
@@ -36,6 +36,12 @@ const pixelateImage = async (photo, size = 1080, pixelSize = 60) => {
   let lastColor;
   const rowCols = [];
 
+  context.clearRect(0, 0, size, size);
+  context.beginPath();
+  context.fillStyle = 'rgb(50,76,47)';
+  context.fillRect(0, 0, size, size);
+  context.closePath();
+
   for (let r = 0; r < numberOfRows; r += 1) {
     for (let c = 0; c < numberOfRows; c += 1) {
       const row = rowFN(r, pixelSize);
@@ -58,38 +64,53 @@ const pixelateImage = async (photo, size = 1080, pixelSize = 60) => {
         startPixel,
         interpolatedColors,
         rgb,
-        interpolate: flip() ? true : false
+        interpolate: false
       });
+
+      // This creates circle pixels
+      context.beginPath();
+      context.fillStyle = `rgb(${rgb.join(',')})`
+      console.log(`rgb(${rgb.join(',')})`)
+      context.arc(
+        c * pixelSize + halfPixel,
+        r * pixelSize + halfPixel,
+        halfPixel - 10,
+        0,
+        2 * Math.PI
+      );
+      context.fill();
       // console.log(interpolatedColors);
 
       // For debugging
-      // data[startPixel] = 255; // red
-      // data[startPixel + 1] = 0; // green
-      // data[startPixel + 2] = 0; // blue
+      data[startPixel] = 255; // red
+      data[startPixel + 1] = 0; // green
+      data[startPixel + 2] = 0; // blue
     }
   }
 
-  rowCols.forEach(
-    ({ startPixel, interpolatedColors, rgb, interpolate }, i, arr) => {
-      for (let x = 0; x < pixelSize; x += 1) {
-        for (let y = 0; y < pixelSize; y += 1) {
-          const _row = rowFN(x, 1);
-          const _col = colFN(y, 1);
-          if (interpolate) {
-            data[startPixel + _row + _col] = interpolatedColors[y][0];
-            data[startPixel + _row + _col + 1] = interpolatedColors[y][1];
-            data[startPixel + _row + _col + 2] = interpolatedColors[y][2];
-          } else {
-            data[startPixel + _row + _col] = rgb[0];
-            data[startPixel + _row + _col + 1] = rgb[1];
-            data[startPixel + _row + _col + 2] = rgb[2];
-          }
-        }
-      }
-    }
-  );
 
-  context.putImageData(imageData, 0, 0);
+  // This does both pixelated image and an interpolated version
+  // rowCols.forEach(
+  //   ({ startPixel, interpolatedColors, rgb, interpolate }, i, arr) => {
+  //     for (let x = 0; x < pixelSize; x += 1) {
+  //       for (let y = 0; y < pixelSize; y += 1) {
+  //         const _row = rowFN(x, 1);
+  //         const _col = colFN(y, 1);
+  //         if (interpolate) {
+  //           data[startPixel + _row + _col] = interpolatedColors[y][0];
+  //           data[startPixel + _row + _col + 1] = interpolatedColors[y][1];
+  //           data[startPixel + _row + _col + 2] = interpolatedColors[y][2];
+  //         } else {
+  //           data[startPixel + _row + _col] = rgb[0];
+  //           data[startPixel + _row + _col + 1] = rgb[1];
+  //           data[startPixel + _row + _col + 2] = rgb[2];
+  //         }
+  //       }
+  //     }
+  //   }
+  // );
+
+  // context.putImageData(imageData, 0, 0);
   // console.log("Image pixelated");
   return { buffer: drawable.toBuffer(), context };
 };
